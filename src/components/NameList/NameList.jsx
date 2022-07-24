a3906c32f7ddfee8d112267b96fb2f48f001fe42
Dushmanthr <dushmantha15ranathunga@gmail.com>
2022-07-24 10:33:20 +0530

    test

---
+++ src/components/NameList/NameList.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function NameListItem(){
+    return(
+        <React.Fragment>
+        <h1>Name List</h1>
+        <hr/>
+        <ul>
+          <li>Dushmantha Ranathunga - NodeJS</li>
+          <li>Lasanthika Sewwandi - ReactJs</li>
+          <li>Hashan Ransingha - Flutter</li>
+  
+  
+        </ul>
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ src/components/NameListItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NameListItem(){
+    retern(
+        <li>Dushmantha Ranathunga - NodeJS</li>
+    )
+}
+
+export default NameListItem;
